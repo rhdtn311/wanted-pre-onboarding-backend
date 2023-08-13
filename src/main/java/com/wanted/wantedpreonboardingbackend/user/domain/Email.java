f05c977267c0db2521ea0a1d6ed588bea15433df
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-08-13 22:20:14 +0900

    feat: Jwt 토큰에서 사용자 이메일을 가져오는 중복 로직 `ArgumentResolver`로 공통 처리 기능 구현
    - 로그인한 사용자 유효성 검증, 토큰에서 email을 파싱하여 가져오는 기능이 중복되기 때문에 `ArgumentResolver`를 커스텀하여 공통 처리하도록 구현

--- src/main/java/com/wanted/wantedpreonboardingbackend/user/domain/Email.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/user/domain/Email.java
@@ -4,9 +4,14 @@ import com.wanted.wantedpreonboardingbackend.user.domain.validation.EmailValid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @EmailValid
 @Embeddable
 public class Email {
